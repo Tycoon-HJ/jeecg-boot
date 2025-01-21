ff843b57b5eee865c0f569c73ab07ab2fb2d5b65
yinhaijun <2523284817@qq.com>
2025-01-21 21:40:06 +0800

    feat:根据关键字查询进行分页查询且按照时间戳倒序进行排序

--- jeecg-boot/system-log/src/main/java/org/jeecg/systemlog/controller/SysTemLogController.java
+++ jeecg-boot/system-log/src/main/java/org/jeecg/systemlog/controller/SysTemLogController.java
@@ -1,5 +1,6 @@
 package org.jeecg.systemlog.controller;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Resource;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -46,17 +48,19 @@ public class SysTemLogController {
     /**
      * 该接口按照时间顺序倒叙，返回最近20条日志信息
      */
-    @GetMapping("/pageLogInfo/{pageNumber}/{pageSize}")
-    public void pageLogInfo(@PathVariable Integer pageNumber, @PathVariable Integer pageSize) {
-        // 只查20条日志信息，默认是按时间戳倒叙
+    @GetMapping("/pageLogInfo/{value}/{pageNumber}/{pageSize}")
+    public List<String> pageLogInfo(@PathVariable String value, @PathVariable Integer pageNumber, @PathVariable Integer pageSize) {
+        List<String> matchMessageList = new ArrayList<>();
+        // 只查20条日志信息，默认是按时间戳倒序
         Query searchQuery = new NativeQueryBuilder()
+                // 根据内容在message字段中查询数据
+                .withQuery(QueryBuilders.match(e -> e.query(value).field("message")))
                 // 分页查询
                 .withPageable(PageRequest.of(pageNumber, pageSize))
-                // 按照时间戳进行倒叙
+                // 按照时间戳进行倒序
                 .withSort(Sort.by("@timestamp").descending())
                 .build();
-        elasticsearchTemplate.search(searchQuery, SystemLog.class).forEach(e -> {
-            System.out.println(e.getContent());
-        });
+        elasticsearchTemplate.search(searchQuery, SystemLog.class).forEach(e -> matchMessageList.add(e.getContent().getMessage()));
+        return matchMessageList;
     }
 }
